2670537e9c79ac101a7007519db7d08ef746844c
bruno34154 <www.brunomesenga@gmail.com>
2021-05-21 07:20:18 -0300

    coluna 5

--- main.js
+++ main.js
@@ -7,3 +7,31 @@ function mostrarAtivo(tag){
    }
       tag.style.color = "#ff0000";
    }
+
+function validar(){
+	var nome = document.getElementById('roundedInput');
+	var email = document.getElementById('roundedEmail');
+
+	if(nome.value == "" & email.value != ""){
+		alert("Nome não informado");
+		nome.focus();
+		return;
+	}
+	if(email.value == "" & nome.value != ""){
+		alert("Email não informado");
+		email.focus();
+		return;
+
+	}
+	if(email.value == "" & nome.value == ""){
+		alert("Nome e email não informados!");
+		email.focus();
+		nome.focus();
+		return;
+		
+	}
+	
+		alert("Dados enviados com sucesso!");
+		
+	
+}
